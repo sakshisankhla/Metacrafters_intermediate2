4554c299ba3d2ad2ce839da724384347f5cfe894
sakshisankhla <148627820+sakshisankhla@users.noreply.github.com>
2024-09-26 15:41:59 +0530

    Update MySimpleToken.sol

--- MySimpleToken.sol
+++ MySimpleToken.sol
@@ -1,72 +1,43 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.2 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-contract MySimpleToken {
-  
-  string public name = "My Simple Token";
-  string public symbol = "MST";
-  uint public decimals = 18;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-  uint public totalTokens;
-  address public owner;
+contract SakshiZeppelin {
+    string public tokenName = "SakshiSankhla";
+    string public tokenAbbr = "SS";
 
+    address owner;
 
-  mapping(address => uint256) public balances;
+    mapping(address => uint) record;
 
-
-  event Transfer(address from, address to, uint amount);
-  event Mint(address to, uint amount);
-  event Burn(address from, uint256 amount);
-  event Approval(address owner, address spender, uint amount);
-
-  constructor(uint startingTokens) {
-    owner = msg.sender;
-    totalTokens = startingTokens;
-    balances[owner] = startingTokens;
-    
-}
-
-  function mint(address to, uint amount) public {
-    if (msg.sender != owner) {
-      revert("Only the owner can create more tokens!");
+    constructor() {
+        owner = msg.sender;
     }
 
-    totalTokens += amount;
-    balances[to] += amount;
-
-    emit Mint(to, amount);
-  }
-
-
-  function burn(uint amount) public {
-
-    if (balances[msg.sender] < amount) {
-        revert("You don't have enough tokens to burn!");
+    function getBalance() external view returns (uint) {
+        return record[msg.sender];
     }
 
-    totalTokens -= amount;
-    balances[msg.sender] -= amount;
-
-    emit Burn(msg.sender , amount); 
-  }
-
-  function approve(address spender, uint allowance) public {
-    balances[spender] = allowance;
-    emit Approval(msg.sender, spender, allowance);
-  }
-
-  function transfer(address to, uint amount) public {
-    if (balances[msg.sender] < amount) {
-      revert("Insufficient balance to send tokens!");
+    // New function to fetch balance by inputting an account address
+    function getBalanceOf(address account) external view returns (uint) {
+        return record[account];
     }
 
-    if (to == address(0)) {
-      revert("Sending tokens to nobody isn't possible!");
+    function mint(address to, uint amount) external {
+        require(msg.sender == owner, "Only owner can mint tokens");
+        record[to] += amount;
     }
 
-    balances[msg.sender] -= amount;
-    balances[to] += amount;
+    function transferTo(address to, uint amount) external {
+        require(record[msg.sender] >= amount, "Insufficient balance in the account");
+        record[to] += amount;
+        record[msg.sender] -= amount;
+    }
 
-    emit Transfer(msg.sender, to, amount);
-  }
+    function burn(uint amount) external {
+        require(record[msg.sender] >= amount, "Insufficient balance in the account");
+        record[msg.sender] -= amount;
+    }
 }
